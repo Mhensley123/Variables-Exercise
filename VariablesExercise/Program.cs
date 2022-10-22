f668a56f0552949ed9d8792b006db24d4a35c224
Maria Hensley <mscelie34@zoho.com>
2022-10-22 04:23:31 -0500

    variables exercise 1

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,18 @@
 ﻿namespace VariablesExercise
 {
     public class Program
-    {
+    {//Variable declarations and initalizations
         static void Main(string[] args)
+
+        string myName = "Maria";
+        int age = 39;
+        char firstLetter = 'm';
+        bool sheIsHungry = true;
+        double weight = 105.7;
+        decimal pizzaPrice = 10.;
         {
             Console.WriteLine("Hello, World!");
+
         }
     }
 }
